d874c2d80e2d866c26c6c23914dda68297ce6ee7
annak1104 <andreynakonechnuy11@gmail.com>
2024-01-20 17:08:38 +0200

    feat: create accounts-table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import './App.css'
+import './App.css';
+import { AccountsTable } from './components/AccountsTable/AccountsTable';
+import accounts from './data/ACCOUNTS_DATA .json';
+import { useMemo } from 'react';
+
+
 export const App = () => {
+  const data = useMemo(() => accounts, []);
+
+  const accountsColumns = [
+    {
+      header: 'ID',
+      accessorKey: 'accountId',
+    },
+    {
+      header: 'email',
+      accessorKey: 'email',
+    },
+    {
+      header: 'authToken',
+      accessorKey: 'authToken',
+    },
+    {
+      header: 'creationDate',
+      accessorKey: 'creationDate',
+    },
+  ];
 
   return (
-    <>Amazon tables</>
+      <AccountsTable accountsColumns={accountsColumns} accounts={data} />
   );
 };
---
+++ src/components/Columns/Columns.ts
@@ -0,0 +1,21 @@
+export const accountsColumns = [
+  {
+    accessorKey: 'id',
+    header: 'accountId',
+  },
+
+  {
+    accessorKey: 'email',
+    header: 'email',
+  },
+
+  {
+    accessorKey: 'authToken',
+    header: 'authToken',
+  },
+
+  {
+    accessorKey: 'creationDate',
+    header: 'creationDate',
+  },
+];
